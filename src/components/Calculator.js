40333b9d19b7611de74183eb9c15233c05d5cc59
Sonika Kumar <sonikak004@gmail.com>
2023-07-19 11:18:20 +0100

    Modify calculator.js with child component buttonrow

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,42 +1,12 @@
-import React from 'react';
 import './Calculator.css';
+import React from 'react';
+import ButtonRow from './rowButton';
 
 function Calculator() {
   return (
     <div className="calculator">
       <div className="display">0</div>
-      <div className="keypad">
-
-        <div className="row">
-          <button className="button" type="button">AC</button>
-          <button className="button" type="button">+/-</button>
-          <button className="button" type="button">%</button>
-          <button className="button right-but" type="button">+</button>
-        </div>
-        <div className="row">
-          <button className="button" type="button">7</button>
-          <button className="button" type="button">8</button>
-          <button className="button" type="button">9</button>
-          <button className="button right-but" type="button">*</button>
-        </div>
-        <div className="row">
-          <button className="button" type="button">4</button>
-          <button className="button" type="button">5</button>
-          <button className="button" type="button">6</button>
-          <button className="button right-but" type="button">-</button>
-        </div>
-        <div className="row">
-          <button className="button" type="button">1</button>
-          <button className="button" type="button">2</button>
-          <button className="button" type="button">3</button>
-          <button className="button right-but" type="button">+</button>
-        </div>
-        <div className="row">
-          <button className="button bottom-button" type="button">0</button>
-          <button className="button" type="button">.</button>
-          <button className="button right-but" type="button">=</button>
-        </div>
-      </div>
+      <ButtonRow />
     </div>
   );
 }
