9b141c6f394e59478b0a29e1367293a4d3bcbfc2
Sonika Kumar <sonikak004@gmail.com>
2023-07-22 19:35:44 +0100

    Add the quotecomponent to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Calculator from './components/Calculator';
 import './App.css';
+import QuoteComponent from './components/QuoteComponent';
 
 function App() {
   return (
     <div className="App">
-      <Calculator />
+      <div className="wrapper">
+        <QuoteComponent />
+        <Calculator />
+      </div>
     </div>
   );
 }
