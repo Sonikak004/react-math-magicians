d98ad33b76e156a58f39988d76aaabfe3dace3c0
Sonika Kumar <sonikak004@gmail.com>
2023-07-27 17:51:37 +0100

    Add test for app.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import '@testing-library/jest-dom/extend-expect';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByRole('heading', { name: /math magician/i });
   expect(linkElement).toBeInTheDocument();
 });
